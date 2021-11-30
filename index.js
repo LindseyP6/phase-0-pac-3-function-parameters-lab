c2e6b8261dce70d948ef502019f71bd1f5ef1841
Lindsey Pregent <lindsey.pregent@gmail.com>
2021-11-30 16:20:53 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    console.log("Hi, my name is ${name}");
+  }
+  introduction();
+
+
+  function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`);
+  }
+
+  function introductionWithLanguageOptional(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`)
+    
+}
